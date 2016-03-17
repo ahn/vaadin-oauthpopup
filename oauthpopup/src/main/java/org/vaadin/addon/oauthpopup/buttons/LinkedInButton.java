0d9efee5296aa0fe0d4b0b7dcca68d2727a9da1c
Bryson Dunn <k.bryson.dunn@gmail.com>
2016-03-17 02:19:38 -0600

    Update OAuthPopupButton subclass constructors

--- oauthpopup/src/main/java/org/vaadin/addon/oauthpopup/buttons/LinkedInButton.java
+++ oauthpopup/src/main/java/org/vaadin/addon/oauthpopup/buttons/LinkedInButton.java
@@ -1,15 +1,16 @@
 package org.vaadin.addon.oauthpopup.buttons;
 
-import org.scribe.builder.api.LinkedInApi;
 import org.vaadin.addon.oauthpopup.OAuthPopupButton;
+import org.vaadin.addon.oauthpopup.OAuthPopupConfig;
 
+import com.github.scribejava.apis.LinkedInApi;
 import com.vaadin.server.ClassResource;
 
 @SuppressWarnings("serial")
 public class LinkedInButton extends OAuthPopupButton {
 	
 	public LinkedInButton(String key, String secret) {
-		super(LinkedInApi.class, key, secret);
+		super(LinkedInApi.instance(), OAuthPopupConfig.getStandardOAuth10aConfig(key, secret));
 		
 		setIcon(new ClassResource("/org/vaadin/addon/oauthpopup/icons/linkedin16.png"));
 		setCaption("LinkedIn");
--- oauthpopup/src/main/java/org/vaadin/addon/oauthpopup/buttons/TwitterButton.java
+++ oauthpopup/src/main/java/org/vaadin/addon/oauthpopup/buttons/TwitterButton.java
@@ -1,15 +1,16 @@
 package org.vaadin.addon.oauthpopup.buttons;
 
-import org.scribe.builder.api.TwitterApi;
 import org.vaadin.addon.oauthpopup.OAuthPopupButton;
+import org.vaadin.addon.oauthpopup.OAuthPopupConfig;
 
+import com.github.scribejava.apis.TwitterApi;
 import com.vaadin.server.ClassResource;
 
 @SuppressWarnings("serial")
 public class TwitterButton extends OAuthPopupButton {
 	
 	public TwitterButton(String key, String secret) {
-		super(TwitterApi.class, key, secret);
+		super(TwitterApi.instance(), OAuthPopupConfig.getStandardOAuth10aConfig(key, secret));
 		
 		setIcon(new ClassResource("/org/vaadin/addon/oauthpopup/icons/twitter16.png"));
 		setCaption("Twitter");
