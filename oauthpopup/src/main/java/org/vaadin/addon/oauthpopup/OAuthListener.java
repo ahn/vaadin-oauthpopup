12e27a210bd3cb9906ad3f3d07f24f13f31adce7
Bryson Dunn <k.bryson.dunn@gmail.com>
2016-03-17 02:27:06 -0600

    Refactor authSuccessful to provide ScribeJava Token object

--- oauthpopup/src/main/java/org/vaadin/addon/oauthpopup/OAuthListener.java
+++ oauthpopup/src/main/java/org/vaadin/addon/oauthpopup/OAuthListener.java
@@ -1,10 +1,12 @@
 package org.vaadin.addon.oauthpopup;
 
+import com.github.scribejava.core.model.Token;
+
 /**
  * Called on OAuth success/failure.
  * <p>
  * In the case that the user doesn't browse back to our application from
- * the authorization url, neither of the methods is called.
+ * the authorization url, neither of the methods are called.
  *
  */
 public interface OAuthListener {
@@ -12,16 +14,18 @@ public interface OAuthListener {
 	/**
 	 * Called on successful OAuth. 
 	 * 
-	 * @param accessToken the OAuth access token
-	 * @param accessTokenSecret the OAuth access token secret
-	 * @param oauthRawResponse the raw response from OAuth2 server. It can contain some useful extended information such as user-id or e-mail.
+	 * @param token The OAuth access token. This will be an {@link OAuth1AccessToken} or {@link OAuth2AccessToken}, 
+	 * depending on the OAuth version of the API.
+	 * @param isOAuth20 true if OAuth 2.0, false if OAuth 1.0a
 	 */
-	public void authSuccessful(String accessToken, String accessTokenSecret, String oauthRawResponse);
+	public void authSuccessful(Token token, boolean isOAuth20);
 	
 	/**
-	 * Called when the OAuth was denied.
+	 * Called when the OAuth was denied. The {@code reason} for authorization failure is the value of the 
+	 * URI query parameter value related to the key specified by {@link OAuthPopupConfig#getErrorParameterName()}.
+	 * A generic error message is retured if the query parameter was not found.
 	 * 
-	 * @param reason TODO: the reasons currently returned are not helpful.
+	 * @param reason The reason for OAuth authorization failure.
 	 */
 	public void authDenied(String reason);
 }
